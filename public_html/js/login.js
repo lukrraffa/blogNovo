e7bb269205a2b23a11b70ddc83f6dbbfde55a893
wlusbar <wlusbar@gmail.com>
2020-01-21 20:50:36 -0300

    js

---
+++ public_html/js/login.js
@@ -0,0 +1,12 @@
+function meuEscopo(){
+    const form=document.querySelector('.form');
+    
+}
+
+const login =[];
+function recebeEventoForm(evento){
+    evento.preventDefault();
+    const userName=form.querySelector('usuario');
+    const senha=form.querySelector('senha');
+    
+}
